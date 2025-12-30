07844e1803ee823ee015b1d13b19bfc4efe3469c
Vitold S <vit1251@gmail.com>
2025-12-30 23:04:08 +0300

    Add more pkg-config base packages

--- tests/test_hash.cpp
+++ tests/test_hash.cpp
@@ -1,18 +1,30 @@
 
 #include <cassert>
 #include <iostream>
+#include <string>
 
 #include "Hash.hpp"
 
-
-int main() {
-    std::cout << "Running tests..." << std::endl;
-
+void test0() {
     Hash hash;
     hash.Update("Hello, world!");
-    auto digest = hash.Digest();
-    assert(digest == "123");
+    std::string digest = hash.Digest();
 
-    std::cout << "All tests passed!" << std::endl;
+    assert(digest == "c1527cd893c124773d811911970c8fe6e857d6df5dc9226bd8a160614c0cd963a4ddea2b94bb7d36021ef9d865d5cea294a82dd49a0bb269f51f6e7a57f79421");
+}
+
+void test1() {
+    Hash *hash = new Hash();
+    hash->Update("Hello, world!");
+    std::string digest = hash->Digest();
+    delete hash;
+
+    assert(digest == "c1527cd893c124773d811911970c8fe6e857d6df5dc9226bd8a160614c0cd963a4ddea2b94bb7d36021ef9d865d5cea294a82dd49a0bb269f51f6e7a57f79421");
+}
+
+
+int main() {
+    test0();
+    test1();
     return 0;
 }
