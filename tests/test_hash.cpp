e1e86d0a18158442e57cc88208c1f0899e9cef64
Vitold S <vit1251@gmail.com>
2025-12-30 23:18:23 +0300

    Add unittest wrappers

--- tests/test_hash.cpp
+++ tests/test_hash.cpp
@@ -3,28 +3,25 @@
 #include <iostream>
 #include <string>
 
+#include <gtest/gtest.h>
+
 #include "Hash.hpp"
 
-void test0() {
+TEST(HashTest, HashOnStack) {
+
     Hash hash;
     hash.Update("Hello, world!");
     std::string digest = hash.Digest();
 
-    assert(digest == "c1527cd893c124773d811911970c8fe6e857d6df5dc9226bd8a160614c0cd963a4ddea2b94bb7d36021ef9d865d5cea294a82dd49a0bb269f51f6e7a57f79421");
+    ASSERT_EQ(digest, "c1527cd893c124773d811911970c8fe6e857d6df5dc9226bd8a160614c0cd963a4ddea2b94bb7d36021ef9d865d5cea294a82dd49a0bb269f51f6e7a57f79421");
+
 }
 
-void test1() {
+TEST(HashTest, HashOnHeap) {
     Hash *hash = new Hash();
     hash->Update("Hello, world!");
     std::string digest = hash->Digest();
     delete hash;
 
-    assert(digest == "c1527cd893c124773d811911970c8fe6e857d6df5dc9226bd8a160614c0cd963a4ddea2b94bb7d36021ef9d865d5cea294a82dd49a0bb269f51f6e7a57f79421");
-}
-
-
-int main() {
-    test0();
-    test1();
-    return 0;
+    ASSERT_EQ(digest, "c1527cd893c124773d811911970c8fe6e857d6df5dc9226bd8a160614c0cd963a4ddea2b94bb7d36021ef9d865d5cea294a82dd49a0bb269f51f6e7a57f79421");
 }
